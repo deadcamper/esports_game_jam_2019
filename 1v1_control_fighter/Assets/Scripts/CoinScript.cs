2e02f7d4a1c2dcf4b5aca8b8e87440e782b9d310
Thomas <ThomasEFerrick@gmail.com>
2019-11-16 03:18:28 -0800

    Level 01 Update

---
+++ 1v1_control_fighter/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(Time.deltaTime * 0, 0, 10));
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+
+
+
+
+
+}
