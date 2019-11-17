0208989cab0c420d86e29b6212a2d1432e7cdedf
deadcamper <ddnewel@gmail.com>
2019-11-16 22:27:43 -0800

    Added a CoinWithWeight, the Spawner, and the TreasureCrate

---
+++ 1v1_control_fighter/Assets/Scripts/Spawner.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject spawnTemplate;
+
+    public bool spawnFirstImmediately;
+
+    public float minimumSpawnDelay;
+    public float maximumSpawnDelay;
+
+    public bool waitUntilLastSpawnedObjectIsDestroyed;
+
+    private float countDown;
+    private GameObject lastSpawnedObject;
+
+    void Start()
+    {
+        if (spawnTemplate == null)
+        {
+            Debug.LogWarning("Template object for spawner was not defined or was destroyed.");
+        }
+
+        if (spawnTemplate.scene.name != null)
+        {
+            spawnTemplate.gameObject.SetActive(false); // Hide!!!
+        }
+
+        if (spawnFirstImmediately)
+        {
+            Spawn();
+        }
+        else
+        {
+            InitializeSpawnCountDown();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CanSpawn())
+        {
+            countDown -= Time.deltaTime * Time.timeScale;
+            if (countDown <= 0)
+            {
+                Spawn();
+            }
+        }
+    }
+
+    private void InitializeSpawnCountDown()
+    {
+        countDown = Random.Range(minimumSpawnDelay, maximumSpawnDelay);
+    }
+
+    public void Spawn()
+    {
+        if (spawnTemplate == null)
+        {
+            Debug.LogWarning("Failed to spawn an object. No template defined.");
+        }
+        else
+        {
+            lastSpawnedObject = Instantiate(spawnTemplate, transform.position, spawnTemplate.transform.rotation, null);
+            lastSpawnedObject.gameObject.SetActive(true);
+        }
+        InitializeSpawnCountDown();
+    }
+
+    private bool CanSpawn()
+    {
+        return (!waitUntilLastSpawnedObjectIsDestroyed || !lastSpawnedObject);
+    }
+
+    private void OnDestroy()
+    {
+        lastSpawnedObject = null;
+    }
+}
---
+++ 1v1_control_fighter/Assets/Scripts/TreasureCrate.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureCrate : MonoBehaviour
+{
+    public Animation animation;
+
+    public float launchSpeed = 10;
+    public int numberOfRewards = 10;
+
+    public GameObject rewardTemplate;
+
+    private Collider2D collide;
+
+    private bool isGifting = false;
+
+    public enum AimDirection
+    {
+        LEFT, RIGHT, FORWARD
+    }
+
+    public AimDirection facing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collide = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator GiftRewards()
+    {
+        animation.Play();
+        while (animation.isPlaying)
+        {
+            yield return null;
+        }
+
+        collide.enabled = false;
+        for (int count = 0; count < numberOfRewards; count++)
+        {
+            GameObject reward = Instantiate(rewardTemplate, transform.position + new Vector3(0, 1f, 0), Quaternion.identity);
+            Rigidbody2D body = reward.GetComponent<Rigidbody2D>();
+
+            if (body)
+            {
+                Vector2 velocity = new Vector2(Random.Range(-1f, 1f), Random.Range(0.25f, 1f));
+                velocity.Normalize();
+                velocity *= launchSpeed;
+
+                if (facing == AimDirection.LEFT)
+                {
+                    velocity.x = -Mathf.Abs(velocity.x);
+                }
+                else if (facing == AimDirection.RIGHT)
+                {
+                    velocity.x = Mathf.Abs(velocity.x);
+                }
+
+                body.velocity = velocity;
+            }
+            else if (count == 0)
+            {
+                Debug.LogWarning("No Rigidbody2D found. Cannot launch reward object.");
+            }
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isGifting && collision.collider.tag == "Player")
+        {
+            StartCoroutine(GiftRewards());
+            isGifting = true;
+        }
+    }
+}
